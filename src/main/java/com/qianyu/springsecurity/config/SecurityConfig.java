df3bc2d10b1d9b5508968166bad06cae61398671
qianyu <mxiao0209@gmail.com>
2023-10-05 12:01:38 +0200

    add securitry config without entity and db

---
+++ src/main/java/com/qianyu/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.qianyu.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	//authentication
+	UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+		UserDetails admin = User
+				.withUsername("admin")
+				.password(
+						passwordEncoder.encode("123")
+						)
+				.roles("ADMIN")
+				.build();
+
+		UserDetails user = User
+				.withUsername("user")
+				.password(
+						passwordEncoder.encode("123")
+						)
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(admin, user);
+
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		//Cross-Site Request Forgery (CSRF) is an attack that forces authenticated users to submit a request to a Web application against which they are currently authenticated.
+		http
+		.csrf(AbstractHttpConfigurer::disable)
+		.authorizeHttpRequests(
+				auth -> auth
+				.requestMatchers( "/welcome")
+				.permitAll()
+				.requestMatchers("/user","/admin")
+				.authenticated()
+				);
+
+		return http.httpBasic(Customizer.withDefaults()).build();
+
+
+
+
+
+
+
+
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+
+}
--- src/main/java/com/qianyu/springsecurity/controller/MessageController.java
+++ src/main/java/com/qianyu/springsecurity/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.qianyu.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,16 @@ public class MessageController {
 	}
 	
 	
-	@GetMapping("/welcomes")
-	public String welcomeS() {
-		return "Welcome Secured";
+	@GetMapping("/user")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public String welcomeU() {
+		return "Welcome User";
+	}
+	
+	@GetMapping("/admin")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public String welcomeA() {
+		return "Welcome Admin";
 	}
 	
 
